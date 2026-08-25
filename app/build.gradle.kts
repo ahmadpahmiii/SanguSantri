41b2ff174ba751dcde984d5f92b392be4e684d53
fahmi <fahmiaisar99@gmail.com>
2026-08-25 10:46:59 +0700

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.sangusantri.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.0.6"
+        versionCode = 13
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "com.sangusantri.app.HiltTestRunner"
 
